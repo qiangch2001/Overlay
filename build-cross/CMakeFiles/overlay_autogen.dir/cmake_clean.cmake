ea1ce5f63ba03ae4425351726c2535a017103d62
qiangch2001 <chenghuanq2001@gmail.com>
2025-12-10 01:38:02 -0500

    Delete useless

--- build-cross/CMakeFiles/overlay_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/overlay_autogen"
-  "overlay_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/overlay_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
